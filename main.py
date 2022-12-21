91e77ca80d4ca9cec020d53344a23e8f323237fa
@rachael.colby <rachael.colby@dharbor.com>
2022-12-21 13:44:09 -0800

    both scripts working

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # This is a sample Python script.
 
 import spacy
+
+
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
@@ -14,19 +16,18 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-
 nlp = spacy.load("en_core_web_sm")
-doc = nlp("This is a sentence.")
-
-print(doc)
-
-# Iterate over tokens in a Doc
-for token in doc:
+doc1 = nlp("This and not to see any money around or being spent this time of year.")
+print(doc1)
+#Iterate over tokens in a Doc
+for token in doc1:
     # Print the text and the predicted part-of-speech tag
     # predictive part of this may not be needed
     print({"text": token.text, "pos": token.pos_, "predictive": token.head.text})
 
-# Iterate over named entities in a Doc
+
+doc = nlp(u"Apple is looking at buying U.K. startup for $1 billion")
+
 for ent in doc.ents:
     print(ent.text, ent.label_)
 
